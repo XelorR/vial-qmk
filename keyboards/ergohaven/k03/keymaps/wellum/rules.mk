c20eabd37f578103d90b4da2dfc58824d41f4d3c
Petr <petr@duck.com>
2024-11-11 21:18:30 +0300

    paths changed to relative, encoders disabled

--- keyboards/ergohaven/k03/keymaps/wellum/rules.mk
+++ keyboards/ergohaven/k03/keymaps/wellum/rules.mk
@@ -1,6 +1,6 @@
 VIAL_ENABLE = yes
-ENCODER_ENABLE = yes
+ENCODER_ENABLE = no
 ENCODER_MAP_ENABLE = yes
 
-SRC += keyboards/ergohaven/k03/keymaps/wellum/oneshot.c
-SRC += keyboards/ergohaven/k03/keymaps/wellum/swapper.c
+SRC += ./oneshot.c
+SRC += ./swapper.c
