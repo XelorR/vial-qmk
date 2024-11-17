fe9f2d2309962f08e2a9931c63f4f0d27834cd0c
Petr <petr@duck.com>
2024-11-17 18:16:50 +0300

    SYM and NUM layers defined

--- keyboards/ergohaven/k03/keymaps/xellum/keymap.c
+++ keyboards/ergohaven/k03/keymaps/xellum/keymap.c
@@ -143,19 +143,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_SYM] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                                       _______, _______, _______, _______, _______, _______,
-        _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),                                       S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______,
-        _______, KC_SCLN, KC_QUOT, KC_GRV,  KC_QUOT, KC_MINS,                                       KC_PLUS, OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  _______,
-        _______, BSL,     DPIPE,   SCLN,    KC_LBRC, KC_UNDS,                                       KC_EQL,  KC_RBRC, CLN,     PIPE,    KC_BSLS, _______,
-                          _______, _______, _______, _______, LA_FN, _______,     _______, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, _______,                                       _______,   _______,    _______, _______, _______, _______,
+        DM_REC1, LG_TILD, LG_AMPR, S(KC_8), S(KC_9), KC_GRV,                                        LG_DQUO,   S(KC_BSLS), KC_O,    LG_LCBR, LG_RCBR, DM_REC2,
+        DM_RSTP, LG_COLON, LG_DLR, S(KC_5), LG_CIRC, S(KC_MINS),                                    S(KC_EQL), OS_SHFT,    OS_CTRL, OS_ALT,  OS_CMD,  DM_RSTP,
+        DM_PLY1, KC_INS,  S(KC_1), LG_AT,   LG_HASH, S(KC_9),                                       S(KC_0),   KC_RBRC,    LG_LT,   LG_GT,   LG_QUES, DM_PLY2,
+                          _______, _______, _______, _______, LA_FN, _______,     _______, _______, _______,   _______,    _______, _______
     ),
 
     [_NUM] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                                         _______, _______, _______, _______, _______, _______,
-        _______, KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,                                           KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   _______,
-        _______, OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, KC_F11,                                          KC_F12,  OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  _______,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-                          _______, _______, _______, _______, _______, _______,     _______, _______, KC_LSFT, _______, _______, _______
+        _______, _______, _______, _______, _______, _______,                                       _______,   _______,    _______,  _______, _______,  _______,
+        DM_REC1, LG_GRAVE, KC_7, KC_8, KC_9, KC_GRV,                                                LG_QUOTE,  S(KC_BSLS), KC_O,     LG_LBR,  LG_RBR,   DM_REC2,
+        DM_RSTP, LG_SCLN, KC_4, KC_5, KC_6, KC_MINS,                                                KC_EQL,    OS_SHFT,    OS_CTRL,  OS_ALT,  OS_CMD,   DM_RSTP,
+        DM_PLY1, KC_0,  KC_1, KC_2,   KC_3, S(KC_9),                                                S(KC_0),   KC_RBRC,    LG_COMMA, LG_DOT,  LG_SLASH, DM_PLY2,
+                          _______, _______, _______, _______, _______, _______,   _______, _______, _______,   _______,    _______,  _______
     ),
 
     [_FN] = LAYOUT(
