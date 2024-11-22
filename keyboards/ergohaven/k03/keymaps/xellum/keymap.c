15115722f0e6c0f51bf59f475b6f6c47ea68b8d8
Petr <petr@duck.com>
2024-11-22 17:43:34 +0300

    KC_NO is even better

--- keyboards/ergohaven/k03/keymaps/xellum/keymap.c
+++ keyboards/ergohaven/k03/keymaps/xellum/keymap.c
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______,  _______,  _______,                                         _______, _______, _______, _______,     _______, _______,
         _______, SPACE_L, SPACE_R, TAB_L,    TAB_R,    SW_TAB,                                          KC_ESC,  KC_HOME, KC_UP,   KC_END,      KC_PGUP, _______,
         KC_TAB,  OS_CMD,  OS_ALT,  OS_CTRL,  OS_SHFT,  SW_WIN,                                          KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT,     KC_PGDN, CW_TOGG,
-        _______, C(KC_Z), C(KC_X), C(KC_C),  C(KC_V),  S(C(KC_Z)),                                      _______, KC_BSPC, KC_DEL,  OSL(_MACRO), KC_APP,  _______,
+        _______, C(KC_Z), C(KC_X), C(KC_C),  C(KC_V),  S(C(KC_Z)),                                      KC_NO,   KC_BSPC, KC_DEL,  OSL(_MACRO), KC_APP,  _______,
                           _______, _______,  _______,  _______, _______, _______,     _______, _______, _______, _______, _______, _______
     ),
 
