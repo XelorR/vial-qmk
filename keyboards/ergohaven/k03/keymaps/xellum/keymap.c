f3007d12ec3c44acc8f83d9dd9e939f71d132896
Petr <petr@duck.com>
2024-11-23 17:52:06 +0300

    columns aligned

--- keyboards/ergohaven/k03/keymaps/xellum/keymap.c
+++ keyboards/ergohaven/k03/keymaps/xellum/keymap.c
@@ -100,11 +100,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_NAV] = LAYOUT(
-        _______,    _______, _______, _______,  _______,  _______,                                         _______, _______, _______, _______, _______,  _______,
-        _______,    SPACE_L, SPACE_R, TAB_L,    TAB_R,    SW_TAB,                                          KC_ESC,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,  _______,
-        C(KC_BSPC), OS_CMD,  OS_ALT,  OS_CTRL,  OS_SHFT,  SW_WIN,                                          KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,  C(KC_DEL),
-        _______,    C(KC_Z), C(KC_X), C(KC_C),  C(KC_V),  OSL(_MACRO),                                     KC_TAB,  KC_F2,   KC_APP,  KC_PSCR, DF(_GAM), _______,
-                             _______, _______,  _______,  _______, _______, _______,     _______, _______, C(KC_PGUP), C(KC_PGDN),    _______, _______
+        _______,    _______, _______, _______,  _______,  _______,                                         _______,    _______,    _______, _______, _______,  _______,
+        _______,    SPACE_L, SPACE_R, TAB_L,    TAB_R,    SW_TAB,                                          KC_ESC,     KC_HOME,    KC_UP,   KC_END,  KC_PGUP,  _______,
+        C(KC_BSPC), OS_CMD,  OS_ALT,  OS_CTRL,  OS_SHFT,  SW_WIN,                                          KC_ENT,     KC_LEFT,    KC_DOWN, KC_RGHT, KC_PGDN,  C(KC_DEL),
+        _______,    C(KC_Z), C(KC_X), C(KC_C),  C(KC_V),  OSL(_MACRO),                                     KC_TAB,     KC_F2,      KC_APP,  KC_PSCR, DF(_GAM), _______,
+                             _______, _______,  _______,  _______, _______, _______,     _______, _______, C(KC_PGUP), C(KC_PGDN), _______, _______
     ),
 
     [_MOUSE] = LAYOUT(
