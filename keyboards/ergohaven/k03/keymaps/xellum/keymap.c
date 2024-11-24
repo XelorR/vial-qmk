1bf91cc433c4acd4713741a78e050e89e8f24482
Petr <petr@duck.com>
2024-11-24 05:41:37 +0300

    typo fixed

--- keyboards/ergohaven/k03/keymaps/xellum/keymap.c
+++ keyboards/ergohaven/k03/keymaps/xellum/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_Q,    KC_W,    KC_F,    KC_P,      KC_B,                                            KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, _______,
         KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,      KC_G,                                            KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_DEL,
         _______, KC_Z,    KC_X,    KC_C,    KC_D,      KC_V,                                            KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-                          _______, _______, LG_TOGGLE, LA_MOUSE, LA_NAV, _______,     _______, LA_SYM,  KC_LSFT, LA_FN,  ____, _______
+                          _______, _______, LG_TOGGLE, LA_MOUSE, LA_NAV, _______,     _______, LA_SYM,  KC_LSFT, LA_FN,   _______, _______
     ),
 
     [_RUS] = LAYOUT(
