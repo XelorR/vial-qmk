a353440205184d7b344783b1f7dcf2e88d72579c
Petr <petr@duck.com>
2024-11-28 15:01:06 +0300

    small alignment

--- keyboards/ergohaven/k03/keymaps/wellum_ruen/keymap.c
+++ keyboards/ergohaven/k03/keymaps/wellum_ruen/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______,  _______,                                         _______, _______, _______, _______, _______, _______,
         _______, SW_TAB,  SW_WIN,  TAB_L,   TAB_R,    KC_ESC,                                          KC_ESC,  KC_HOME, KC_END,  KC_BSPC, KC_DEL,  _______,
         _______, OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,  KC_ENT,                                          KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, SPACE_L, SPACE_R, DF(_GAM), KC_PSCR, KC_TAB,                                         KC_TAB,  KC_PGUP, KC_PGDN, KC_QUOT, KC_NUM,  _______,
+        _______, SPACE_L, SPACE_R, DF(_GAM), KC_PSCR, KC_TAB,                                          KC_TAB,  KC_PGUP, KC_PGDN, KC_QUOT, KC_NUM,  _______,
                           _______, _______, _______,  _______, _______, _______,     _______, _______, _______, _______, _______, _______
     ),
 
