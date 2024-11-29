37b74a64009d71af173ce465393a1844ec26cdfc
Petr <petr@duck.com>
2024-11-29 20:16:46 +0300

    next / prev word and delete word added

--- keyboards/ergohaven/k03/keymaps/xellum/keymap.c
+++ keyboards/ergohaven/k03/keymaps/xellum/keymap.c
@@ -95,11 +95,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_NAV] = LAYOUT(
-        _______, _______, _______, _______,  _______, _______,                                         _______, _______, _______, _______, _______, _______,
-        KC_VOLU, SW_TAB,  SW_WIN,  TAB_L,    TAB_R,   KC_ESC,                                          KC_ESC,  KC_WH_U, KC_WH_D, KC_BSPC, KC_DEL,  KC_PSCR,
-        KC_VOLD, OS_CMD,  OS_ALT,  OS_CTRL,  OS_SHFT, KC_ENT,                                          KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, A(KC_PSCR),
-        KC_MUTE, SPACE_L, SPACE_R, DF(_GAM), KC_BTN1, KC_TAB,                                          KC_TAB,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  G(S(KC_S)),
-                          _______, _______,  _______, _______, _______, _______,     _______, _______, KC_BTN1, CW_TOGG, _______, _______
+        _______, _______, _______, _______,  _______, _______,                                         _______, _______,  _______,    _______, _______, _______,
+        KC_VOLU, SW_TAB,  SW_WIN,  TAB_L,    TAB_R,   KC_ESC,                                          KC_ESC,  DF(_GAM), C(KC_BSPC), KC_BSPC, KC_DEL,  KC_PSCR,
+        KC_VOLD, OS_CMD,  OS_ALT,  OS_CTRL,  OS_SHFT, KC_ENT,                                          KC_ENT,  KC_LEFT,  KC_DOWN,    KC_UP,   KC_RGHT, A(KC_PSCR),
+        KC_MUTE, SPACE_L, SPACE_R, KC_PGUP,  KC_PGDN, KC_TAB,                                          KC_TAB,  KC_HOME,  WRD_PRV,    WRD_NXT, KC_END,  G(S(KC_S)),
+                          _______, _______,  _______, _______, _______, _______,     _______, _______, KC_BTN1, CW_TOGG,  _______,    _______
     ),
 
     [_NUM] = LAYOUT(
