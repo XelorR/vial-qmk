2a10c957633f55cdd54b5ca954fa41e22ecbf699
Petr <petr@duck.com>
2024-12-06 18:59:25 +0300

    eng layer aligned with new invented layout

--- keyboards/ergohaven/k03/keymaps/xellum/keymap.c
+++ keyboards/ergohaven/k03/keymaps/xellum/keymap.c
@@ -55,11 +55,11 @@ enum keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ENG] = LAYOUT(
-        LG_TILD,  LG_GRAVE, LG_DQUO, LG_HASH,  LG_DLR,    LG_PERC,                                       LG_CIRC, LG_AMPR, S(KC_8), S(KC_9), S(KC_0), LG_AT,
-        MC_4,     KC_Q,     KC_W,    KC_F,     KC_P,      KC_B,                                          KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, MC_5,
-        MC_6,     KC_A,     KC_R,    KC_S,     KC_T,      KC_G,                                          KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    MC_7,
-        LG_COLON, KC_Z,     KC_X,    KC_C,     KC_D,      KC_V,                                          KC_K,    KC_H,    KC_COMM, KC_DOT,  LG_PIPE, LG_SLASH,
-                            LG_LT,   LG_GT,    LG_TOGGLE, KC_SPC, LA_NAV, MC_10,          MC_13, LA_SYM, OSM_SFT, CW_TOGG, KC_MINS, KC_EQL
+        KC_NO, KC_1, KC_2,  KC_3,  KC_4,  KC_5,                             KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_NO,
+        KC_NO, KC_Q, KC_W,  KC_F,  KC_P,  KC_B,                             KC_J,    KC_L,   KC_U,    KC_Y,   KC_QUOT, KC_NO,
+        KC_NO, KC_A, KC_R,  KC_S,  KC_T,  KC_G,                             KC_M,    KC_N,   KC_E,    KC_I,   KC_O,    KC_NO,
+        KC_NO, KC_Z, KC_X,  KC_C,  KC_D,  KC_V,                             KC_K,    KC_H,   KC_COMM, KC_DOT, KC_SLSH, KC_NO,
+                     KC_NO, KC_NO, KC_NO, LA_NAV, LA_NUM, KC_NO,     KC_NO, LA_SYM, OSM_SFT, KC_NO, KC_NO, KC_NO
     ),
 
     [_RUS] = LAYOUT(
