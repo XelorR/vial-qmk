4a629c1ce09995437946efeb3225e2437e15369e
Petr <petr@duck.com>
2024-12-13 12:57:29 +0300

    new , . position

--- keyboards/ergohaven/k03/keymaps/xellum/keymap.c
+++ keyboards/ergohaven/k03/keymaps/xellum/keymap.c
@@ -89,16 +89,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ENG] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                            KC_6,     KC_7,          KC_8,      KC_9,    KC_0,    KC_BSPC,
         KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                            KC_J,     KC_L,          KC_U,      KC_Y,    KC_QUOT, KC_DEL,
-        KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                            KC_M,     KC_N,          KC_E,      KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT, MO_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                            KC_K,     KC_H,          KC_COMM,   KC_DOT,  LG_DOT,  OSM(MOD_LSFT),
-                          KC_LGUI, KC_LALT, KC_LCTL, KC_SPC, LA_NAV, LG_SET_RU,    LG_SET_EN, LA_SYM, LG_COMMA, OSM(MOD_RCTL), KC_LBRC,   KC_RBRC
+      LG_COMMA,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                            KC_M,     KC_N,          KC_E,      KC_I,    KC_O,    LG_DOT,
+      LG_SET_RU, MO_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                            KC_K,     KC_H,          KC_COMM,   KC_DOT,  LG_DOT,  LG_SET_EN,
+                          KC_LGUI, KC_LALT, KC_LCTL, KC_SPC, LA_NAV, KC_NO,            KC_NO, LA_SYM, OSM(MOD_LSFT), OSM(MOD_RCTL), KC_LBRC,   KC_RBRC
     ),
 
     [_RUS] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                                         _______, _______, _______, _______, _______, _______,
         _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
-        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        _______, MO_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                            KC_N,    KC_M,    KC_COMM, KC_DOT,  LG_DOT,  _______,
+       LG_COMMA, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LG_DOT,
+        _______, MO_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT, _______,
                           _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______
     ),
 
