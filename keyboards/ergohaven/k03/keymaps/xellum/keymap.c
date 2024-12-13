8881dfb1f383aebe0907f5aa0e08592dd4f932c3
Petr <petr@duck.com>
2024-12-13 22:16:36 +0300

    square brackets moved to english layer

--- keyboards/ergohaven/k03/keymaps/xellum/keymap.c
+++ keyboards/ergohaven/k03/keymaps/xellum/keymap.c
@@ -87,10 +87,10 @@ enum keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ENG] = LAYOUT(
-       _______,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,                                            KC_6,          KC_7,      KC_8,    KC_9,   KC_0,    _______,
-       _______,  KC_Q,    KC_W,    KC_F,    KC_P,      KC_B,                                            KC_J,          KC_L,      KC_U,    KC_Y,   KC_QUOT, _______,
-       _______,  KC_A,    KC_R,    KC_S,    KC_T,      KC_G,                                            KC_M,          KC_N,      KC_E,    KC_I,   KC_O,    _______,
-       _______,  MO_Z,    KC_X,    KC_C,    KC_D,      KC_V,                                            KC_K,          KC_H,      KC_COMM, KC_DOT, KC_SCLN, _______,
+       _______,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,                                            KC_6,          KC_7,      KC_8,    KC_9,    KC_0,    _______,
+       _______,  KC_Q,    KC_W,    KC_F,    KC_P,      KC_B,                                            KC_J,          KC_L,      KC_U,    KC_Y,    KC_QUOT, _______,
+       _______,  KC_A,    KC_R,    KC_S,    KC_T,      KC_G,                                            KC_M,          KC_N,      KC_E,    KC_I,    KC_O,    _______,
+       _______,  MO_Z,    KC_X,    KC_C,    KC_D,      KC_V,                                            KC_K,          KC_H,      KC_LBRC, KC_RBRC, KC_SCLN, _______,
                           _______, _______, LG_SET_RU, KC_SPC, LA_NAV, KC_NO,            KC_NO, LA_SYM, OSM(MOD_LSFT), LG_SET_EN, _______, _______
     ),
 
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                             KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
         _______, CL_WIN,  SW_WIN,  TAB_L,   TAB_R,   KC_ESC,                                            KC_ESC,  KC_HOME, KC_UP,   KC_END,  KC_DEL,  _______,
         _______, OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, KC_ENT,                                            KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, _______,
-        _______, SPACE_L, SPACE_R, LG_WORD, CW_TOGG, KC_TAB,                                            KC_TAB,  KC_PGUP, KC_PGDN, KC_LBRC, KC_RBRC, _______,
+        _______, SPACE_L, SPACE_R, LG_WORD, CW_TOGG, KC_TAB,                                            KC_TAB,  KC_PGUP, KC_PGDN, KC_APP,  KC_PSCR, _______,
                           _______, _______, _______, _______, _______, _______,       _______, LA_FUN, KC_BTN1,  _______, _______, _______
     ),
 
