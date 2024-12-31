abd58eaf7bf5e5dbfc1919c947cd841539633c5c
Petr <petr@duck.com>
2024-12-31 05:46:03 +0300

    osm replaced with ossm

--- keyboards/ergohaven/k03/keymaps/xellum/keymap.c
+++ keyboards/ergohaven/k03/keymaps/xellum/keymap.c
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SYM] = LAYOUT(
         KC_NO,   KC_NO,      KC_NO,   KC_NO,    KC_NO,    KC_NO,                                               KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO,   UC(0x2014), KC_LT,   KC_EQL,   KC_GT,    KC_GRV,                                              KC_CIRC, KC_LBRC, KC_UNDS, KC_RBRC, LG_NUM,  KC_NO,
-        KC_NO,   KC_BSLS,    KC_LPRN, KC_MINS,  KC_RPRN,  KC_PLUS,                                             KC_DQT,  OSM_SFT, OSM_CTL, OSM_AGR, OSM_GUI, KC_NO,
+        KC_NO,   KC_BSLS,    KC_LPRN, KC_MINS,  KC_RPRN,  KC_PLUS,                                             KC_DQT,  OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_NO,
         KC_NO,   KC_HASH,    KC_ASTR, KC_COLN,  KC_SLSH,  KC_DLR,                                              KC_PERC, KC_PIPE, KC_TILD, KC_AMPR, KC_SCLN, KC_NO,
                              KC_NO,   KC_NO,    KC_ESC,   KC_SPC,  KC_TAB, KC_NO,            KC_NO,   _______, _______, _______, KC_NO,   KC_NO
     ),
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SYMRU] = LAYOUT(
         KC_NO, KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,                                               KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO, UC(0x2014), LG_LT,   KC_EQL,  LG_GT,   KC_GRV,                                              LG_CIRC, KC_LBRC, KC_UNDS, KC_RBRC, KC_HASH, KC_NO,
-        KC_NO, KC_BSLS,    KC_LPRN, KC_MINS, KC_RPRN, KC_PLUS,                                             S(KC_2), OSM_SFT, OSM_CTL, OSM_AGR, OSM_GUI, KC_NO,
+        KC_NO, KC_BSLS,    KC_LPRN, KC_MINS, KC_RPRN, KC_PLUS,                                             S(KC_2), OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_NO,
         KC_NO, LG_HASH,    KC_ASTR, S(KC_6), KC_PIPE, LG_DLR,                                              KC_PERC, LG_PIPE, LG_TILD, LG_AMPR, S(KC_4), KC_NO,
                            KC_NO,   KC_NO,   KC_ESC,  KC_SPC,  KC_TAB, KC_NO,            KC_NO,   _______, _______, _______, KC_NO,   KC_NO
     ),
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM] = LAYOUT(
         KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                                              KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,
         KC_NO, KC_EQL,  KC_7,    KC_8,    KC_9,     LG_SLASH,                                           KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,
-        KC_NO, KC_PLUS, KC_4,    KC_5,    KC_6,     KC_ENT,                                             KC_NO,   OSM_SFT, OSM_CTL,  OSM_AGR, OSM_GUI, KC_NO,
+        KC_NO, KC_PLUS, KC_4,    KC_5,    KC_6,     KC_ENT,                                             KC_NO,   OS_SHFT, OS_CTRL,  OS_ALT,  OS_CMD,  KC_NO,
         KC_NO, KC_MINS, KC_1,    KC_2,    KC_3,     KC_ASTR,                                            KC_NO,   KC_EXLM, LG_COMMA, LG_DOT,  LG_QUES, KC_NO,
                         KC_NO,   KC_NO,   _______,  KC_0,     _______, KC_NO,           KC_NO, _______, _______, _______, KC_NO,    KC_NO
     ),
@@ -158,7 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                             KC_NO,    KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,
         KC_NO,   CL_WIN,  SW_WIN,  TAB_L,   TAB_R,   KC_ESC,                                            KC_ESC,   KC_HOME, KC_UP,   KC_END,    KC_PGUP, KC_NO,
-        KC_NO,   OSM_GUI, OSM_ALT, OSM_CTL, OSM_SFT, KC_ENT,                                            KC_ENT,   KC_LEFT, KC_DOWN, KC_RGHT,   KC_PGDN, KC_NO,
+        KC_NO,   OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, KC_ENT,                                            KC_ENT,   KC_LEFT, KC_DOWN, KC_RGHT,   KC_PGDN, KC_NO,
         KC_NO,   UNDO,    CUT,     COPY,    PASTE,   KC_TAB,                                            KC_TAB,   CW_TOGG, LG_WORD, G(KC_SPC), KC_LALT, KC_NO,
                           KC_NO,   KC_NO,   _______, _______, _______, KC_NO,           KC_NO, KC_ENT,  KC_BSPC,  KC_DEL,  KC_NO,   KC_NO
     ),
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN] = LAYOUT(
         KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,                                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO, KC_F12,  KC_F7, KC_F8, KC_F9,   KC_PSCR,                                   DM_REC1, _______, _______, _______, QK_BOOT, KC_NO,
-        KC_NO, KC_F11,  KC_F4, KC_F5, KC_F6,   KC_SCRL,                                   DM_RSTP, OSM_SFT, OSM_CTL, OSM_AGR, OSM_GUI, KC_NO,
+        KC_NO, KC_F11,  KC_F4, KC_F5, KC_F6,   KC_SCRL,                                   DM_RSTP, OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD, KC_NO,
         KC_NO, KC_F10,  KC_F1, KC_F2, KC_F3,   KC_CAPS,                                   DM_PLY1, PASTE,   COPY,    CUT,     UNDO,    KC_NO,
                         KC_NO, KC_NO, _______, _______, _______, KC_NO,   KC_NO, _______, _______, _______, KC_NO, KC_NO
     ),
@@ -174,7 +174,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MOUSE] = LAYOUT(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO,   KC_DEL,  KC_NO,   KC_NO,   KC_NO,   KC_NO,                                        KC_NO,   KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U, KC_NO,
-        KC_NO,   OSM_GUI, OSM_ALT, OSM_CTL, OSM_SFT, KC_NO,                                        KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_NO,
+        KC_NO,   OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, KC_NO,                                        KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_NO,
         KC_NO,   UNDO,    CUT,     COPY,    PASTE,   KC_NO,                                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                           KC_NO,   KC_NO,   _______, _______, _______, KC_NO,     KC_NO,  KC_BTN2, KC_BTN1, KC_BTN3, KC_NO,   KC_NO
     ),
@@ -182,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MEDIA] = LAYOUT(
         KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO,  QK_BOOT, _______, _______, _______, KC_VOLU,                                         UC_LINX, _______, KC_VOLU, _______, _______, KC_NO,
-        KC_NO,  OSM_GUI, OSM_ALT, OSM_CTL, OSM_SFT, KC_VOLD,                                         UC_WINC, KC_MPRV, KC_VOLD, KC_MNXT, CG_TOGG, KC_NO,
+        KC_NO,  OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, KC_VOLD,                                         UC_WINC, KC_MPRV, KC_VOLD, KC_MNXT, CG_TOGG, KC_NO,
         KC_NO,  UNDO,    CUT,     COPY,    PASTE,   KC_MUTE,                                         UC_MAC,  _______, KC_MUTE, _______, _______, KC_NO,
                          KC_NO,   KC_NO,   _______, _______, _______, _______,     _______, KC_MSTP, KC_MPLY, KC_MUTE, KC_NO,   KC_NO
     ),
