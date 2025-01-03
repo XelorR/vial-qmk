c28ac7cec72cb5a03544a95c890e42d67301d802
Petr <petr@duck.com>
2025-01-03 09:27:31 +0300

    duplicate game toggle removed from mouse layer

--- keyboards/ergohaven/k03/keymaps/miryoku/keymap.c
+++ keyboards/ergohaven/k03/keymaps/miryoku/keymap.c
@@ -114,11 +114,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_MOUSE] = LAYOUT(
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   QK_BOOT, LA_ALT,  KC_NO,   KC_NO,    KC_NO,                                        KC_NO,   KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U, KC_NO,
-        KC_NO,   KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,  KC_NO,                                        KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_NO,
-        KC_NO,   KC_NO,   KC_ALGR, LA_GAM,  LA_MAC,   KC_NO,                                        REDO,    PASTE,   COPY,    CUT,     UNDO,    KC_NO,
-                          KC_NO,   KC_NO,   DF(_GAM), _______, _______, KC_NO,     KC_NO,  KC_BTN2, KC_BTN1, KC_BTN3, KC_NO,   KC_NO
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   QK_BOOT, LA_ALT,  KC_NO,   KC_NO,   KC_NO,                                        KC_NO,   KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U, KC_NO,
+        KC_NO,   KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                                        KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_NO,
+        KC_NO,   KC_NO,   KC_ALGR, LA_GAM,  LA_MAC,  KC_NO,                                        REDO,    PASTE,   COPY,    CUT,     UNDO,    KC_NO,
+                          KC_NO,   KC_NO,   _______, _______, _______, KC_NO,     KC_NO,  KC_BTN2, KC_BTN1, KC_BTN3, KC_NO,   KC_NO
     ),
 
     [_SYM] = LAYOUT(
