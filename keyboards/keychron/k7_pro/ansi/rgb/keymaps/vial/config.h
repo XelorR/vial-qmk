aa8fe0562278eab02a6723719efef657f95709ee
Petr <petr@duck.com>
2025-01-18 19:45:54 +0300

    keyboard id generated, other config.h options copypasted from k6pro ansi
    command to generate unique ID:
    python3 util/vial_generate_keyboard_uid.py

---
+++ keyboards/keychron/k7_pro/ansi/rgb/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x40, 0x73, 0xD9, 0xD2, 0xB5, 0x21, 0xC1, 0x14}
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+// #define MIDI_ADVANCED
